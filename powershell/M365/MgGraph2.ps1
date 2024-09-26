22b5e1ab056c82970430e03604ef1c7102e2ed87
sptrains <pssreddy01@gmail.com>
2024-09-26 12:11:20 +0530

    Commit

---
+++ powershell/M365/MgGraph2.ps1
@@ -0,0 +1,13 @@
+﻿
+$cId="2f6dde73-eb69-4089-bc4e-6ef06b828c0e"
+$tId = "c518f592-d3fe-405a-b17d-456bcf93a271"
+
+$ClientSecretCredential = Get-Credential -Credential $cId
+# Enter client_secret in the password prompt.
+Connect-MgGraph -TenantId $tId -ClientSecretCredential $ClientSecretCredential
+
+Connect-MgGraph -ClientId $cId -TenantId $tId
+
+Get-MgSite
+
+Connect-MgGraph -Identity
